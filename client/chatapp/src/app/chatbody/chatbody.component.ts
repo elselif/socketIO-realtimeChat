68ef6d01bce613ec51efa9fc35c0f27af1bfc4c4
Elif <2003013054@student.beykent.edu.tr>
2023-08-04 15:54:18 +0300

    -

--- client/chatapp/src/app/chatbody/chatbody.component.ts
+++ client/chatapp/src/app/chatbody/chatbody.component.ts
@@ -1,48 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ChatServiceService } from '../chat/chat-service.service';
+import { Subscription } from 'rxjs';
 
 @Component({
-selector: 'app-chatbody',
-templateUrl: './chatbody.component.html',
-styleUrls: ['./chatbody.component.css']
+  selector: 'app-chatbody',
+  templateUrl: './chatbody.component.html',
+  styleUrls: ['./chatbody.component.css']
 })
-export class ChatbodyComponent implements OnInit{
-user:String = '';
-room:String = '';
-messageText:String = '';
-messageArray:Array<{user:String,message:String}> = [];
+export class ChatbodyComponent implements OnInit, OnDestroy {
+  user: String = '';
+  room: String = '';
+  messageText: String = '';
+  messages: Array<{ user: String, message: String }> = [];
+  private messageSubscription: Subscription = new Subscription;
 
-constructor(private router : Router,private _chatService : ChatServiceService){
-this._chatService.newUserJoined()
-.subscribe(data=> this.messageArray.push(data));
+  constructor(private router: Router, private _chatService: ChatServiceService) {
+    this._chatService.newUserJoined()
+      .subscribe(data => this.messages.push(data));
 
+    this._chatService.userLeftRoom()
+      .subscribe(data => this.messages.push(data));
 
-this._chatService.userLeftRoom()
-.subscribe(data=>this.messageArray.push(data));
+    this.messageSubscription = this._chatService.newMessageReceived()
+      .subscribe(data => this.messages.push(data));
+  }
 
-this._chatService.newMessageReceived()
-.subscribe(data=>this.messageArray.push(data));
-}
+  ngOnInit(): void { }
 
+  ngOnDestroy(): void {
+    this.messageSubscription.unsubscribe();
+  }
 
-ngOnInit(): void {
+  sendMessage() {
+    const data = { user: this.user, room: this.room, message: this.messageText };
+    this._chatService.sendMessage(data);
+    this.messageText = ''; // Gönderdikten sonra mesaj alanını temizleme işlemi
+  }
 
-}
+  join() {
+    this._chatService.joinRoom({ user: this.user, room: this.room });
+  }
 
-
-
-
-join(){
-this._chatService.joinRoom({user:this.user, room:this.room});
-}
-
-leave(){
-this._chatService.leaveRoom({user:this.user, room:this.room});
-}
-
-sendMessage()
-{
-this._chatService.sendMessage({user:this.user, room:this.room, message:this.messageText});
-}
+  leave() {
+    this._chatService.leaveRoom({ user: this.user, room: this.room });
+  }
 }
